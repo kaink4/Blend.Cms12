6728d2337f89ded69810aa639dc9ef2bbd508bd7
Karol Bajda <karol.bajda@pl.abb.com>
2022-11-05 20:54:59 +0100

    initial commit

---
+++ Blend.Cms12/Models/Pages/GeneralPage.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blend.Cms12.Models.Pages
+{
+    [ContentType(
+        DisplayName = "General Page",
+        Description = "General Page description",
+        GroupName = "General Content",
+        GUID = "f1d33bf5-6f4b-47ec-8de6-bf0dd827862f")]
+    public class GeneralPage : PageData
+    {
+        [Display(
+            Name = "Headline",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Headline { get; set; }
+
+        [Display(
+            Name = "HeadlineSlug",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual string HeadlineSlug { get; set; }
+
+        [Display(
+            Name = "Body",
+            GroupName = SystemTabNames.Content,
+            Order = 30)]
+        public virtual XhtmlString Body { get; set; }
+    }
+}
